38dc10d66949876a741ebc9cc0b58ae540fcb707
Thiago Bruno <tyagobruno@live.com>
2022-11-18 19:53:54 -0300

    fix

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -13,7 +13,7 @@ const TermsPage: React.FC = () => {
         textAlign="center"
         p="20px"
         alignSelf="center"
-        w={['full', 'full']}
+        w={['full', '800px']}
         bg="rgb(0,0,0,0.05)"
         borderRadius="4px"
       >
