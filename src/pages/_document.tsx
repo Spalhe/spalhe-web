dc0e24352fc1f06eb5359e9ed3e6f4da15444151
Thiago Bruno Miranda <thiagobrunomiranda@gmail.com>
2023-09-13 09:08:59 -0300

    add google ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,14 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode="light" />
           <Main />
           <NextScript />
+
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11317294690"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11317294690');
+          </script>
         </body>
       </Html>
     )
