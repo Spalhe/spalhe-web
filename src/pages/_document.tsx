66e84610d32f2880875e5e8582dea7d66041aa23
Thiago Bruno Miranda <thiagobrunomiranda@gmail.com>
2024-07-27 00:52:59 -0300

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default class Document extends NextDocument {
   render() {
     return (
@@ -10,14 +11,6 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode="light" />
           <Main />
           <NextScript />
-
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11317294690"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-11317294690');
-          </script>
         </body>
       </Html>
     )
